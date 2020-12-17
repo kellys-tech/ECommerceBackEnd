35f1b9da0ad48bd0a7169c93a0ca046bf195d082
Kelly Smith <kdalton77@hotmail.com>
2020-12-17 18:45:00 -0500

    fixed bug in tag route for put request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,14 +48,14 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const updateTag = await Tag.update({
-      where: {
-        id: req.params.id
-      }
-    });
+    const updateTag = await Tag.update({tag_name: req.body.tag_name}, {where: {id: req.params.id}})
+    if (!updateTag) {
+      res.status(404).json({message: 'No tag found with that id'})
+      return;
+    }
     res.status(200).json(updateTag);
   } catch (err) {
-    res.status(400).json;
+    res.status(400).json(err);
   }
 });
 
